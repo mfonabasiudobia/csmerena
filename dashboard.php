8f3980beeefa459d6c8e33cf24b9c7456fcccb64
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-05-09 11:04:22 +0100

    Update

--- dashboard.php
+++ dashboard.php
@@ -16,29 +16,31 @@
         <?php require_once("inc/top-ads.php"); ?>
 
         <section>
-            <div class="container bg-white">
+            <div class="container">
                 
-                <div class="row gx-4">
+                <div class="row g-3">
                     
                     <?php require_once("inc/dashboard-sidebar.php"); ?>
 
-                    <div class="col-lg-8 order-1 order-md-2 bg-white border p-0 view-news">
+                    <div class="col-lg-9 view-news">
 
-                    <main class="p-3">
-                    
-                        <div class="row">
+                    <main class="p-3 d-grid gap-3 border bg-white">
+                        
+                     <section>
+                            <h1>Dashboard</h1>
+                        <div class="row g-3">
                             <section class="col-md-4">
-                                <div class="bg-warning p-2 text-white">
-                                    <section class="d-flex justify-content-between align-items-center">
+                                <div class="bg-primary text-white">
+                                    <section class="d-flex px-3 p-2 justify-content-between align-items-center">
                                         <div class="left">
                                             <h1>2</h1>
                                             <div>Orders</div>
                                         </div>
                                         <div class="right">
-                                            <i class="fas fa-shopping-cart fs-30"></i>
+                                            <i class="text-transparent-black fas fa-shopping-cart fa-4x"></i>
                                         </div>
                                     </section>
-                                    <a href="#" class="d-block text-decoration-none text-white fs-13 py-1 text-center">
+                                    <a href="#" class="bg-transparent-black  d-block text-decoration-none text-white fs-13 py-1 text-center">
                                         <span>More Info</span>
                                         <i class="fas fa-arrow-circle-right"></i>
                                     </a>
@@ -46,17 +48,17 @@
                             </section>
 
                              <section class="col-md-4">
-                                <div class="bg-success p-2 text-white">
-                                    <section class="d-flex justify-content-between align-items-center">
+                                <div class="bg-success text-white">
+                                    <section class="d-flex px-3 p-2 justify-content-between align-items-center">
                                         <div class="left">
                                             <h1>₦0</h1>
                                             <div>Earnings</div>
                                         </div>
                                         <div class="right">
-                                            <i class="fas fa-money fs-30"></i>
+                                            <i class="text-transparent-black fas fa-money-bill-alt fa-4x"></i>
                                         </div>
                                     </section>
-                                    <a href="#" class="d-block text-decoration-none text-white fs-13 py-1 text-center">
+                                    <a href="#" class="bg-transparent-black  d-block text-decoration-none text-white fs-13 py-1 text-center">
                                         <span>More Info</span>
                                         <i class="fas fa-arrow-circle-right"></i>
                                     </a>
@@ -65,23 +67,262 @@
 
 
                              <section class="col-md-4">
-                                <div class="bg-danger p-2 text-white">
-                                    <section class="d-flex justify-content-between align-items-center">
+                                <div class="bg-danger text-white">
+                                    <section class="d-flex px-3 p-2 justify-content-between align-items-center">
                                         <div class="left">
                                             <h1>2</h1>
                                             <div>SMS Units</div>
                                         </div>
                                         <div class="right">
-                                            <i class="fas fa-shopping-cart fs-30"></i>
+                                            <i class="text-transparent-black fas fa-mobile-alt fa-4x"></i>
                                         </div>
                                     </section>
-                                    <a href="#" class="d-block text-decoration-none text-white fs-13 py-1 text-center">
+                                    <a href="#" class="bg-transparent-black  d-block text-decoration-none text-white fs-13 py-1 text-center">
                                         <span>More Info</span>
                                         <i class="fas fa-arrow-circle-right"></i>
                                     </a>
                                 </div>
                             </section>
                         </div>
+                        </section>
+
+                        <section>
+                            
+
+                     <ul class="nav nav-tabs bg-gray-100 pt-2 ps-2 border" id="myTab" role="tablist">
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link active text-gray-500" id="inbox-tab" data-bs-toggle="tab" data-bs-target="#inbox" type="button" role="tab" aria-controls="inbox" aria-selected="true">Inbox</button>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link text-gray-500" id="notification-tab" data-bs-toggle="tab" data-bs-target="#notification" type="button" role="tab" aria-controls="notification" aria-selected="false">Notification</button>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link text-gray-500" id="friend-request-tab" data-bs-toggle="tab" data-bs-target="#friend-request" type="button" role="tab" aria-controls="friend-request" aria-selected="false">Friend Request</button>
+                      </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+
+                      <div class="tab-pane p-3 fade show active" id="inbox" role="tabpanel">
+                         <table class="table table-striped fs-13">
+                              <thead class="">
+                                  <tr>
+                                      <th class="border-0 border-top">Message</th>
+                                      <th class="border-0 border-top">Sender</th>
+                                      <th class="border-0 border-top">Sent On</th>
+                                  </tr>
+                              </thead>
+
+                              <tbody>
+                                  <tr>
+                                      <td>
+                                          <a href="#">New Message from Connect2Me</a>
+                                      </td>
+                                      <td>
+                                          <a href="#">Alimi</a>
+                                      </td>
+                                      <td>
+                                          5-11-2019 9:33 AM
+                                      </td>
+                                  </tr>
+
+                                   <tr>
+                                      <td>
+                                          <a href="#">New Message from Connect2Me</a>
+                                      </td>
+                                      <td>
+                                          <a href="#">Alimi</a>
+                                      </td>
+                                      <td>
+                                          5-11-2019 9:33 AM
+                                      </td>
+                                  </tr>
+
+                                  <tr>
+                                      <td>
+                                          <a href="#">New Message from Connect2Me</a>
+                                      </td>
+                                      <td>
+                                          <a href="#">Alimi</a>
+                                      </td>
+                                      <td>
+                                          5-11-2019 9:33 AM
+                                      </td>
+                                  </tr>
+                              </tbody>
+                          </table>
+
+                          <div class="d-flex justify-content-end my-3">
+                                 <a href="post-comment.php" class="load-more bg-red-900 text-white">ALL MESSAGES &#187;</a>
+                            </div>
+
+                      </div>
+
+
+
+                      <div class="tab-pane fade" id="notification" role="tabpanel">
+                          
+                          <table class="table table-striped fs-13">
+                              <thead class="">
+                                  <tr>
+                                      <th class="border-0 border-top"><input type="checkbox" /></th>
+                                      <th class="border-0 border-top">Post</th>
+                                      <th class="border-0 border-top"># Comments    </th>
+                                      <th class="border-0 border-top"></th>
+                                  </tr>
+                              </thead>
+
+                              <tbody>
+                                  <tr>
+                                     <td>
+                                         <input type="checkbox" />
+                                     </td>
+                                      <td>
+                                          <a href="#">Final Week: Myschool CBT challenge season 8 - N500,000 cash prizes, free airtime & activation pins - see instructions</a>
+                                      </td>
+                                      <td>
+                                          92 Comments
+                                      </td>
+                                      <td>
+                                          <button class="btn btn-danger btn-sm fs-14">Delete</button>
+                                      </td>
+                                  </tr>
+
+
+                                   <tr>
+                                     <td>
+                                         <input type="checkbox" />
+                                     </td>
+                                      <td>
+                                          <a href="#">Final Week: Myschool CBT challenge season 8 - N500,000 cash prizes, free airtime & activation pins - see instructions</a>
+                                      </td>
+                                      <td>
+                                          92 Comments
+                                      </td>
+                                      <td>
+                                          <button class="btn btn-danger btn-sm fs-14">Delete</button>
+                                      </td>
+                                  </tr>
+
+
+
+
+                                 
+                              </tbody>
+                          </table>
+
+                          <div class="d-flex justify-content-end my-3">
+                                 <a href="post-comment.php" class="load-more bg-red-900 text-white">ALL NOTIFICATIONS &#187;</a>
+                            </div>
+
+
+                      </div>
+                      <div class="tab-pane fade" id="friend-request" role="tabpanel" >
+                          
+                           <table class="table table-striped fs-13">     
+                              <tbody>
+                                  <tr>
+                                     <td scope="row">
+                                         <img src="assets/images/gsmarena_00.jpg" >
+                                     </td>
+                                      <td>
+                                          <a href="#">Kinsley</a>
+                                          <div>IBBU Male Student - 300 level</div>
+                                      </td>
+                                      <td>
+                                          <button class="btn btn-danger btn-sm fs-14">Delete Request</button>
+                                      </td>
+                                  </tr>
+
+                                 
+                              </tbody>
+                          </table>
+
+                          <div class="d-flex justify-content-end my-3">
+                                 <a href="post-comment.php" class="load-more bg-red-900 text-white">ALL FRIEND REQUEST &#187;</a>
+                            </div>
+
+                      </div>
+                    </div>
+
+
+                        </section>
+
+
+                        <section class="row">
+                             <div class="col-md-6">
+                                 <div class="card coloured-links border">
+                                      <div class="card-header">
+                                        Group Recommendations
+                                      </div>
+                                      <ul class="list-group list-group-flush">
+                                        <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3" >
+                                            <a href="#">Medicine and surgery uniuyo</a>
+                                        </li>
+
+                                        <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3" >
+                                            <a href="#">Medicine and surgery uniuyo</a>
+                                        </li>
+
+                                        <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3" >
+                                            <a href="#">Medicine and surgery uniuyo</a>
+                                        </li>
+
+                                        <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3" >
+                                            <a href="#">Medicine and surgery uniuyo</a>
+                                        </li>
+
+                                        <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3" >
+                                            <a href="#">Medicine and surgery uniuyo</a>
+                                        </li>
+
+                                        <li class="list-group-item d-flex py-3">
+                                            <a href="post-comment.php" class="load-more bg-red-900 text-white me-3">FIND A GROUP</a>
+                                            <a href="post-comment.php" class="load-more bg-red-900 text-white me-3">START A GROUP</a>
+                                        </li>
+                                      </ul>
+                                    </div>
+                             </div>
+
+
+                               <div class="col-md-6">
+                                 <div class="card coloured-links border">
+                                      <div class="card-header">
+                                        Connect2me
+                                      </div>
+                                      <ul class="list-group list-group-flush">
+                                        <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3 img-thumbnail" >
+                                            <div>
+                                                <a href="#">Kingsley</a> <span> (f ) UNIUYO aspirant</span>
+                                            </div>
+                                        </li>
+
+                                         <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3 img-thumbnail" >
+                                            <div>
+                                                <a href="#">Kingsley</a> <span> (f ) UNIUYO aspirant</span>
+                                            </div>
+                                        </li>
+
+                                         <li class="list-group-item d-flex ">
+                                            <img src="assets/images/gsmarena_00.jpg" class="me-3 img-thumbnail" >
+                                            <div>
+                                                <a href="#">Kingsley</a> <span> (f ) UNIUYO aspirant</span>
+                                            </div>
+                                        </li>
+
+                                        <li class="list-group-item d-flex py-3">
+                                            <a href="post-comment.php" class="load-more bg-red-900 text-white me-3">FIND A FRIEND</a>
+                                        </li>
+                                      </ul>
+                                    </div>
+                             </div>
+                        </section>
 
                     </main>
 
--- inc/dashboard-sidebar.php
+++ inc/dashboard-sidebar.php
@@ -1,117 +1,118 @@
-<div class="col-lg-4 overflow-hidden order-2 order-md-1 p-2">
-                        <main class="row gy-3 bg-white py-2">
-                            <section>
-                            <h6 class="content-title oswald-font fs-20">Marketplace</h6>
+<div class="col-lg-3 overflow-hidden" x-data="{ show : true}">
 
-                              <main class="row gy-2 p-0">
+<div class="d-block d-md-none">
+    <a class="btn p-0 text-decoration-none" x-on:click='show = !show'>
+      <i class="fas fa-bars border p-2 bg-white"></i>
+      <span>Dashboard Menu</span>
+    </a>
+</div>
+<div class="accordion accordion-flush border dashboard-sidebar" :class="show ? 'd-none d-md-block' : 'd-block'">
 
-                            <?php
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <a href='#' class="accordion-button remove-arrow collapsed d-flex" >
+        <div>
+          <i class="fas fa-th"></i> 
+        </div>
+        <span>Dashboard</span>
+      </a>
+    </h2>
+  </div>
 
-                               foreach ([1,2,3] as $key => $value) {
-                                  echo  "
-                                <div class='card bg-white p-0 ps-3'>
-                                              <div class='row g-0'>
-                                                <div class='col-4'>
-                                                  <img src='assets/images/gsmarena_002.jpg' class='w-100 h-100' alt='...'>
-                                                </div>
-                                                <div class='col-8'>
-                                                  <div class='card-body py-0'>
-                                                    <h5 class='card-title oswald-font lh-1'><a href='view_news.php' class='stretched-link fs-14 fw-bold'>ppo Reno8 Lite leaks: a rebranded Reno7 Lite for Europe</a></h5>
-                                                    <div class='card-text fs-13'>
-                                                        <span>&#8358;5,000</span>
-                                                        <span class='bg-success px-1 rounded text-light'>UNIBEN</span>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed d-flex" type="button" data-bs-toggle="collapse" data-bs-target="#flush-blog">
+        <div>
+          <i class="fas fa-comment-alt"></i> 
+        </div>
 
-                                                        <span class='bg-danger px-1 rounded text-light'>New</span>
-                                                    </div>
-                                                  </div>
-                                                </div>
-                                              </div>
-                                </div>";
+        <span>Blog</span>
+      </button>
+    </h2>
+    <div id="flush-blog" class="accordion-collapse collapse" >
+      <div class="accordion-body">
+        <ul class="list-style-none fs-14 d-grid gap-2">
+          <li>
+            <a href="#">
+              <i class="fas fa-chevron-right me-2"></i>
+              <span>View Post</span>
+            </a>
+          </li>
+          <li>
+             <a href="#">
+              <i class="fas fa-chevron-right me-2"></i>
+              <span>Create Post</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+ 
+   <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed d-flex" type="button" data-bs-toggle="collapse" data-bs-target="#flush-events">
+         <div>
+          <i class="far fa-calendar-alt "></i>
+        </div>
 
-                            }
+         <span>Events</span>
+      </button>
+    </h2>
+    <div id="flush-events" class="accordion-collapse collapse" >
+      <div class="accordion-body">
+        <ul class="list-style-none fs-14 d-grid gap-2">
+          <li>
+            <a href="#">
+              <i class="fas fa-chevron-right me-2"></i>
+              <span>View Post</span>
+            </a>
+          </li>
+          <li>
+             <a href="#">
+              <i class="fas fa-chevron-right me-2"></i>
+              <span>Create Post</span>
+            </a>
+          </li>
+        </ul>
+        </ul>
+      </div>
+    </div>
+  </div>
 
-                                ?>
 
-                              </main>
+   <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed d-flex" type="button" data-bs-toggle="collapse" data-bs-target="#flush-users">
+         <div>
+          <i class="fas fa-users "></i>
+        </div>
 
-                              <div class="d-flex justify-content-end my-3">
-                                 <a href="post-comment.php" class="load-more bg-red-900 text-white">VIEW MORE &#187;</a>
-                            </div>
+         <span>Users</span>
+      </button>
+    </h2>
+    <div id="flush-users" class="accordion-collapse collapse" >
+      <div class="accordion-body">
+        <ul class="list-style-none fs-14 d-grid gap-2">
+          <li>
+            <a href="#">
+              <i class="fas fa-chevron-right me-2"></i>
+              <span>View Post</span>
+            </a>
+          </li>
+          <li>
+             <a href="#">
+              <i class="fas fa-chevron-right me-2"></i>
+              <span>Create Post</span>
+            </a>
+          </li>
+        </ul>
+        </ul>
+      </div>
+    </div>
+  </div>
 
-                        </section>
 
+</div>
 
-                        <section>
-                            <h6 class="content-title oswald-font fs-20">QUICK LINKS</h6>
-
-                              <main class="px-2">
-
-                            <?php
-
-                               foreach ([1,2,3,4,5] as $key => $value) {
-
-                                  echo  "<h5 class='lh-1 border-bottom oswald-font py-2'><a href='view_news.php' class='fs-14 fw-bold'>&#187; Ppo Reno8 Lite leaks</a></h5>";
-
-                                }
-
-                                ?>
-
-                              </main>
-
-                        </section>
-
-                        <section>
-                            <h6 class="content-title oswald-font fs-20">UPCOMING EVENTS</h6>
-
-                              <main class="row gy-2 p-0">
-
-                            <?php
-
-                               foreach ([1,2,3] as $key => $value) {
-                                  echo  "
-                                <div class='card bg-white p-0 ps-3'>
-                                              <div class='row g-0'>
-                                                <div class='col-3 fs-10 pt-3 pb-1 bg-danger d-flex flex-column justify-content-center align-items-center text-white'>
-                                                    <h6 class='fs-16 lh-0 fw-bold'>6th</h6>
-                                                    <div>May</div>
-                                                    <div>2022</div>
-                                                </div>
-                                                <div class='col-9'>
-                                                  <div class='card-body py-0'>
-                                                    <h5 class='card-title lh-1'>
-                                                    <a href='view_news.php' class='stretched-link oswald-font fs-14 fw-bold'>JAMB exam for 2022 UTME candidates begins</a></h5>
-                                                  </div>
-                                                </div>
-                                              </div>
-                                </div>";
-
-                            }
-
-                                ?>
-
-                              </main>
-
-                               <div class="d-flex justify-content-end my-3">
-                                 <a href="post-comment.php" class="load-more bg-red-900 text-white">VIEW MORE &#187;</a>
-                            </div>
-                        </section>
-
-                         <section>
-                                <h6 class="oswald-font fs-20 content-title mb-2">NEWSLETTER</h6>
-
-                              <form class="row gy-2 p-0">
-
-                                <div class="col-12">
-                                    <label class="fs-12 fw-light">Subscribe to newsletter</label>
-                                    <input class="form-control" />
-                                </div>
-
-                                 <div class="col-12 d-flex justify-content-end">
-                                    <button class="btn oswald-font bg-gray-500 text-white">SUBSCRIBE</button>
-                                </div>
-
-                              </form>
-                        </section>
-
-                        </main>
 </div>
