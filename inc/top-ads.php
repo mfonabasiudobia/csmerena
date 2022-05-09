a80d9caf836c2189bf4311340c47ad22280fd6f9
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-05-09 20:47:52 +0100

    Update

--- inc/top-ads.php
+++ inc/top-ads.php
@@ -1,5 +1,5 @@
 
-<section class="container d-grid gap-1 pb-3 fs-14">
+<section class="container d-grid gap-1 pb-3 fs-14 bg-white my-2 card card-body">
     <div class="d-flex justify-content-center"><span class="oswald-font fw-bold text-gray-500 me-2 text-nowrap">SPECIAL:</span> <a href="#" class="text-primary">JAMB introduces new texts for language subjects and Literature-in-English</a></div>
 
     <div class="d-flex justify-content-center"><span class="oswald-font fw-bold text-gray-500 me-2 text-nowrap">FREE UPDATES:</span> <a href="#" class="text-primary">JAMB mock exam questions for 2022 UTME</a></div>
