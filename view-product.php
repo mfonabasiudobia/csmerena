110701978cfbc6ca880d6db708c496b6c68bc274
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-05-09 13:48:11 +0100

    Update

--- view-product.php
+++ view-product.php
@@ -18,54 +18,95 @@
         <section>
             <div class="container overflow-hidden">
                 
+                <h2>Reno8 Lite leaks: a rebranded Reno7 Lite for Europe</h2>
                 <div class="row gx-4">
                     
                     <section class="col-lg-8 px-0 order-2 order-md-1">
                         <main class="d-grid gap-3 px-2 bg-white border py-3">
-                            
                             <section>
-                                <h2>ppo Reno8 Lite leaks: a rebranded Reno7 Lite for Europe</h2>
-
-                                <div class="row py-3">
+                                <div class="row  gy-3">
                                     <div class="col-md-4">
                                         <div class="h-100 d-flex justify-content-center">
                                             <img src='assets/images/gsmarena_00.jpg' class="img-fluid mx-auto h-100 object-cover" />
                                         </div>
                                     </div>
                                     <div class="col-md-8">
-                                        <div>
-                                                <div class="d-flex align-items-center border-bottom py-2">
-                                                        <div class="me-2">Price:</div> 
-                                                        <div class="fs-20 fw-bold oswald-font">&#8358; 5000</div>
-                                                </div>
+                                        <table class="table table-bordered fs-14">
+                                                <tr>
+                                                        <td>Price:</td> 
+                                                        <td>&#8358; 5000</td>
+                                                </tr>
 
-                                                <div class="d-flex align-items-center border-bottom py-2">
-                                                        <div class="me-2">School:</div> 
-                                                        <div>University of Medical Sciences, Ondo State</div>
-                                                </div>
+                                                <tr>
+                                                        <td>School:</td> 
+                                                        <td><a href="#">University of Medical Sciences, Ondo State</a></td>
+                                                </tr>
 
-                                                <div class="d-flex align-items-center border-bottom py-2">
-                                                        <div class="me-2">Availability:</div> 
-                                                        <div class="fs-20 fw-bold oswald-font">Instock</div>
-                                                </div>
-                                                <div class="d-flex align-items-center border-bottom py-2">
-                                                        <div class="me-2">Category</div> 
-                                                        <div>Tutorials & Lessons</div>
-                                                </div>
-                                                <div class="d-flex align-items-center border-bottom py-2">
-                                                        <div class="me-2">Date Posted:</div> 
-                                                        <div>12th April 2022</div>
-                                                </div>
-                                        </div>
+                                                <tr>
+                                                        <td>Availability:</td> 
+                                                        <td>Instock</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Category</td> 
+                                                        <td>Tutorials & Lessons</td>
+                                                </tr>
+                                                <tr>
+                                                        <td>Date Posted:</td> 
+                                                        <td>12th April 2022</td>
+                                                </tr>
+                                        </table>
                                     </div>    
                                 </div>
+
+                                <section class="my-3">
+                                   <div class="alert alert-warning" role="alert">
+                                      <h5 class="alert-heading">WARNING!</h5>
+
+                                      <ol class="fs-14">
+                                          <li>Y If you understand the above warnings, please contact the seller using the details below.</li>
+                                          <li>Don't make payment until you see the item.</li>
+                                          <li> If you understand the above warnings, please contact the seller using the details below.</li>
+                                          <li> If you understand the above warnings, please contact the seller using the details below.</li>
+                                      </ol>
+                                    </div>
+                                 </section>
 
                                 <section class="blog-content py-2">
                                     <h5 class="oswald-font py-0 lh-0">Item Description</h5>
 
                                     <p>The Black and Gray choices for Z Fold4 are not new - we had them on the Z Fold3 was announced, although they were called Phantom Black and Phantom Silver. Looking at the clamshell, Black is getting lighter to the point of arriving as Gray, the Cream option will be named Gold, and the Lavender is staying.</p>
+
+
+                                     <div class="d-flex justify-content-center">
+                                         <img src='assets/images/gsmarena_00.jpg' class="img-fluid block mx-auto object-cover" />
+                                     </div>
                                       
                                   </section>
+
+                                <section class="my-3">
+                                   <div class="alert alert-warning" role="alert">
+                                      <h5 class="alert-heading">WARNING!</h5>
+
+                                      <ol class="fs-14">
+                                          <li>Y If you understand the above warnings, please contact the seller using the details below.</li>
+                                          <li>Don't make payment until you see the item.</li>
+                                          <li> If you understand the above warnings, please contact the seller using the details below.</li>
+                                          <li> If you understand the above warnings, please contact the seller using the details below.</li>
+                                      </ol>
+                                    </div>
+                                 </section>
+
+
+                                 <section class="my-3">
+                                    <div class="col-12 oswald-font mb-2">
+                                        Contact Collinsn:
+                                    </div>
+
+                                    <button class="btn btn-lg btn-success">
+                                        <i class="fa fa-phone me-2"></i>
+                                        <span>093084849393</span>
+                                    </button>
+                                </section>
 
 
                                 <form class="row gy-3 py-3">
@@ -85,7 +126,7 @@
                                 </form>
                             </section>
 
-                            <section class="row gy-3">
+                            <section class="row gy-3 card-list related-ads">
                                 <h5 class="oswald-font py-0 lh-0">Related Adverts</h5>
                          <?php
 
@@ -93,17 +134,17 @@
                                   echo  "
                                 <div class='card bg-white p-0 ps-3'>
                                               <div class='row g-0'>
-                                                <div class='col-4 col-md-2'>
-                                                  <img src='assets/images/gsmarena_002.jpg' class='w-100 h-100' alt='...'>
+                                                <div class='col-4 col-md-2 left'>
+                                                  <img src='assets/images/gsmarena_002.jpg' class='img-thumbnail' alt='...'>
                                                 </div>
-                                                <div class='col-8 ol-md-10'>
-                                                  <div class='card-body py-0'>
-                                                    <h5 class='card-title lh-1'><a href='view_news.php' class='stretched-link fs-14 fw-bold'>ppo Reno8 Lite leaks: a rebranded Reno7 Lite for Europe</a></h5>
+                                                <div class='col-8 col-md-10'>
+                                                  <div class='card-body py-3'>
+                                                    <h5 class='card-title lh-1'><a href='view_news.php' class='stretched-link'>Reno8 Lite leaks: a rebranded Reno7 Lite for Europe</a></h5>
                                                     <div class='card-text fs-13'>
-                                                        <span>&#8358;5,000</span>
+                                                        <span class='text-danger'>&#8358;5,000</span>
                                                         <span class='bg-success px-1 rounded text-light'>UNIBEN</span>
 
-                                                        <span class='bg-danger px-1 rounded text-light'>New</span>
+                                                        <span class='bg-warning px-1 rounded text-light'>New</span>
                                                     </div>
                                                   </div>
                                                 </div>
